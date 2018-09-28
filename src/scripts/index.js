f4c114b2520b98db617575d4a3b6fcb9a9ac4f5d
Liak Wee <llee9@sapient.com>
2018-09-28 15:17:28 +0800

    Added UIKit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,10 @@
 import 'babel-polyfill';
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 import printMe from './modules/print.js';
 import '../styles/style.scss';
 
 console.log('START');
 printMe();
+UIkit.use(Icons);
+UIkit.notification('Hello world.');
